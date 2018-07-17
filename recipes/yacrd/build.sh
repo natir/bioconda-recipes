b0b084e6ade026bd40858ddf0323db9990ccd585
Pierre Marijon <pierre@marijon.fr>
2018-07-18 01:46:20 +0200

    Update yacrd recipes

--- recipes/yacrd/build.sh
+++ recipes/yacrd/build.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
 
-mkdir -p $PREFIX/bin
+# circumvent a bug in conda-build >=2.1.18,<3.0.10
+# https://github.com/conda/conda-build/issues/2255
+[[ -z $REQUESTS_CA_BUNDLE && ${REQUESTS_CA_BUNDLE+x} ]] && unset REQUESTS_CA_BUNDLE
+[[ -z $SSL_CERT_FILE && ${SSL_CERT_FILE+x} ]] && unset SSL_CERT_FILE
 
-mkdir build
-cd build
-cmake ..
-make -j 8
-
-cp yacrd $PREFIX/bin/
-
+# build statically linked binary with Rust
+cargo install --root $PREFIX --verbose
